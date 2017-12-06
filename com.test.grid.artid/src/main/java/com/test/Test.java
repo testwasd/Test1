880f7b0a54f93fd5b0e68c4916e161e76fba8e08
testwasd <34271711+testwasd@users.noreply.github.com>
2017-12-06 14:22:55 +0530

    Add files via upload

---
+++ com.test.grid.artid/src/main/java/com/test/Test.java
@@ -0,0 +1,26 @@
+package com.test; 
+
+/**
+ * @author 167619
+ *
+ */
+public class Test {
+
+	/**
+	 * 
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("**********************************");		
+		
+
+	}
+
+}
